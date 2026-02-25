a7fd2bf18986aeae676635e9d9ae1d007f9918fc
Tahmid79274 <tahmidur.rahman.79274@gmail.com>
2026-02-26 00:45:48 +0600

    need to work more

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
 def main():
-    print("Hello from ai-agent!")
+    # print("Hello from ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+    client = genai.Client(api_key=api_key)
+    # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    response = client.models.generate_content(model="gemini-2.5-flash",contents=args.user_prompt)
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
 
 
 if __name__ == "__main__":
