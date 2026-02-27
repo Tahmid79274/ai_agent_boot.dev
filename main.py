e44f67bf0a394f4fa490d12ad5e0d43e21549710
Tahmid <tahmiudr.rahman.79274@gmail.com>
2026-02-27 14:33:02 +0600

    mulitple messages need to reviewed.

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import argparse
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 def main():
-    # print("Hello from ai-agent!")
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
     client = genai.Client(api_key=api_key)
     # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model="gemini-2.5-flash",contents=args.user_prompt)
-    print(f"User prompt: {args.user_prompt}")
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
+    print(f"User prompt: {messages}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
