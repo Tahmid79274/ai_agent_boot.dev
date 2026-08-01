70ff09f4b764c5297162c3ab3595763a4aed3610
Tahmid79274 <tahmidur.rahman.79274@gmail.com>
2026-08-02 01:59:56 +0600

    arg parse

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
-<<<<<<< HEAD
     client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=api_key,
@@ -27,20 +26,6 @@ def main():
     print(f"Prompt tokens: {response.usage.prompt_tokens}")
     print(f"Response tokens: {response.usage.completion_tokens}")
     print(f"Response: {response.choices[0].message.content}")
-=======
-    client = genai.Client(api_key=api_key)
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.5-flash",contents=messages)
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response: {response.text}")
-    
-
-    
-    
->>>>>>> 314364f5a927f7b3e748266cd62e6b739dda769c
 
 
 if __name__ == "__main__":
