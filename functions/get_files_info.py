5825df8e6b786cf6e064131701fc99707f1b57c2
Tahmid79274 <tahmidur.rahman.79274@gmail.com>
2026-08-03 23:28:55 +0600

    testing

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,7 +2,7 @@ import os
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     working_dir_abs = os.path.abspath(working_directory)
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    # print(f"Absolute Path: {working_dir_abs}")
+    print(f"Absolute Path: {working_dir_abs}")
     # print(f"target_dir: {target_dir}")
     # Will be True or False
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
