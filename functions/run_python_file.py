04d70f8d90966c47d0f28f4324f62844a169dc99
Tahmid <tahmiudr.rahman.79274@gmail.com>
2026-08-20 21:41:07 +0600

    now it can run python scripts.

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -7,6 +7,7 @@ def run_python_file(working_directory: str, file_path: str, args: list[str] | No
         target_dir_with_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
         # print(f'Target File with full dir: {target_dir_with_path}')
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir_with_path]) == working_dir_abs
+        # print(f'Trying to run for {target_dir_with_path}')
         if not valid_target_dir:
             return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
         if not os.path.isfile(target_dir_with_path):
@@ -15,21 +16,24 @@ def run_python_file(working_directory: str, file_path: str, args: list[str] | No
             return f'Error: "{file_path}" is not a Python file'
         else:
             command = ["python", target_dir_with_path]
-            # if len(args) > 0:
-            command.extend(args)
-            completed_process = subprocess.run(args=command, stdin=None, input=None, stdout=working_dir_abs, stderr=None, capture_output=True, shell=False, cwd=working_dir_abs, timeout=30, check=False, encoding=None, errors=None, text=True, env=None, universal_newlines=None,)
-            print(f'{file_path}\'s Return Code: {completed_process.returnCode}')
-            print(f'{file_path}\'s Std Out: {completed_process.stdout}')
-            print(f'{file_path}\'s Std Err: {completed_process.stderr}')
-            # output_str = ''
-            # if completed_process.returnCode != 0:
-            #     output_str += f"Process exited with code {completed_process.returnCode}"
-            # if len(completed_process.stdout) == 0 and len(completed_process.stderr) == 0:
-            #     output_str += f"No output produced"
-            # elif len(completed_process.stdout) != 0:
-            #     output_str += f"STDOUT:{completed_process.stdout}"
-            # elif len(completed_process.stdout) != 0:
-            #     output_str += f"STDERR:{completed_process.stderr}"
-            # return output_str
+            # print(f'Arguments: {args}')
+            if args != None:
+                command.extend(args)
+            # print(f'Extended Arguments: {command}')
+            completed_process = subprocess.run(args=command, stdin=None, input=None,  capture_output=True, shell=False, cwd=working_dir_abs, timeout=30, check=False, encoding=None, errors=None, text=True, env=None, universal_newlines=None,)
+            # print(f'{file_path}\'s Return Code: {completed_process.returncode}')
+            # print(f'{file_path}\'s Std Out: {type(completed_process.stdout)}')
+            # print(f'{file_path}\'s Std Err: {type(completed_process.stderr)}')
+            output_str = ''
+            if completed_process.returncode != 0:
+                output_str += f"Process exited with code {completed_process.returnCode}"
+            if len(completed_process.stdout) == 0 and len(completed_process.stderr) == 0:
+                output_str += f"No output produced"
+            if len(completed_process.stdout) != 0:
+                output_str += f"STDOUT:{completed_process.stdout}"
+            if len(completed_process.stdout) != 0:
+                output_str += f"STDERR:{completed_process.stderr}"
+            
+            return output_str
     except Exception as e:
         return f"Error: executing Python file: {e}"
